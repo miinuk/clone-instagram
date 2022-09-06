6a2b6561fa5f063feccde0c8b9e6171a2e402983
willian <willianpereira39@hotmail.com>
2022-09-06 19:15:20 -0300

    criação do projeto

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import {db} from './firebase.js';
+import {useEffect} from 'react';
+function App() {
+  
+
+    useEffect( ()=>{
+
+      console.log(db);
+      asjsajasjasasjjasjasas
+
+    },[])
+    return(
+      <div className="App">
+          
+      </div>
+    );
+
+
+
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+
+const firebaseApp = firebase.initializeApp( {
+    apiKey: "AIzaSyDv2tqdcUeLlFy9egSKCRbkNWzDcKQB9PI",
+    authDomain: "cloneinstagram-2e0f3.firebaseapp.com",
+    projectId: "cloneinstagram-2e0f3",
+    storageBucket: "cloneinstagram-2e0f3.appspot.com",
+    messagingSenderId: "411760376696",
+    appId: "1:411760376696:web:c9725bdd8eef6361fe668d",
+    measurementId: "G-DLPH1L7WZT"
+});
+
+
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage().ref();
+const functions = firebase.functions();
+
+export {db,auth,storage};
